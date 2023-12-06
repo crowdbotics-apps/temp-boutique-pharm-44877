6d93373f4261480bd9ce59cf6b994fbc99d87e79
Crowdbotics <team@crowdbotics.com>
2023-12-06 13:49:05 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "temp-boutique-pharm-44877",
+  url: "https://temp-boutique-pharm-44877.botics.co",
+  api: "https://temp-boutique-pharm-44877.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
